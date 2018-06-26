d3124aac4f22a963f521d242187a1bb8fc86e27c
Matthew Richardson <matthew.richardson@ncfunds.com>
2018-06-26 10:26:33 -0400

    dev

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const preloader = document.querySelector('.preload');
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   preloader.classList.add('visible');
+});
+
+document.getElementById('btnRSVP').addEventListener('click', function () {
+  document.getElementById('home').className = 'fadeout';
+  setTimeout(function () {
+    document.getElementById('rsvp-form').className = 'fadein';
+  }, 1000);
 });
